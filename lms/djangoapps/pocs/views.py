addbc7ea26ffae07980936dbba436a48a376393a
Chris Rossi <chris@archimedeanco.com>
2014-11-14 15:40:10 -0500

    Enforce POC Coach role.

--- lms/djangoapps/pocs/views.py
+++ lms/djangoapps/pocs/views.py
@@ -1,20 +1,45 @@
+import functools
+
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
+from django.utils.translation import ugettext as _
 from django.views.decorators.cache import cache_control
 from django_future.csrf import ensure_csrf_cookie
 
 from courseware.courses import get_course_by_id
 from edxmako.shortcuts import render_to_response
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
+from student.roles import CoursePocCoachRole
 
 from .models import PersonalOnlineCourse
 
 
+def coach_dashboard(view):
+    """
+    View decorator which enforces that the user have the POC coach role on the
+    given course and goes ahead and translates the course_id from the Django
+    route into a course object.
+    """
+    @functools.wraps(view)
+    def wrapper(request, course_id):
+        course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+        role = CoursePocCoachRole(course_key)
+        if not role.has_user(request.user):
+            return HttpResponseForbidden(
+                _('You must be a POC Coach to access this view.'))
+        course = get_course_by_id(course_key, depth=None)
+        return view(request, course)
+    return wrapper
+
+
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
-def dashboard(request, course_id):
-    course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
-    course = get_course_by_id(course_key, depth=None)
+@coach_dashboard
+def dashboard(request, course):
+    """
+    Display the POC Coach Dashboard.
+    """
     poc = get_poc_for_coach(course, request.user)
     context = {
         'course': course,
@@ -22,22 +47,24 @@ def dashboard(request, course_id):
     }
     if not poc:
         context['create_poc_url'] = reverse(
-            'create_poc', kwargs={'course_id': course_id})
+            'create_poc', kwargs={'course_id': course.id})
     return render_to_response('pocs/coach_dashboard.html', context)
 
 
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
-def create_poc(request, course_id):
-    course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
-    course = get_course_by_id(course_key, depth=None)
+@coach_dashboard
+def create_poc(request, course):
+    """
+    Create a new POC
+    """
     name = request.POST.get('name')
     poc = PersonalOnlineCourse(
         course_id=course.id,
         coach=request.user,
         display_name=name)
     poc.save()
-    url = reverse('poc_coach_dashboard', kwargs={'course_id': course_id})
+    url = reverse('poc_coach_dashboard', kwargs={'course_id': course.id})
     return redirect(url)
 
 
